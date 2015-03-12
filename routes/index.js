5d4c7ffbe136e3355ac9bef7be2de5fa36305955
Falco Nogatz <fnogatz@gmail.com>
2015-03-12 14:57:42 +0100

    Use JavaScript Standard Style

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,15 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Express' })
+})
 
-
-router.get('/map/:dataset', function(req, res, next) {
+router.get('/map/:dataset', function (req, res, next) {
   res.render('map', {
     title: 'Map'
-  });
-});
+  })
+})
 
-
-module.exports = router;
+module.exports = router
